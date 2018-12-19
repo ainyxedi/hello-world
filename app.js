dac77c396a779dd67672377e586916e6b57d5fe7
Ainy Zaidi <ainyxedi@gmail.com>
2018-12-19 19:35:48 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+/*console.log("Hello World");
+console.log(123);
+var greeting = 'hello';
+console.log (greeting);
+console.log([1,2,3]);
+console.log({a:1, b:2, c:3});
+console.table({a:1, b:2, c:3});
+
+const person = {name: 'Sara', age : 30, profession: 'Chef'};
+console.log(person);
+person.name = 'HAnnah';
+person.age = 38;
+person.profession = 'Consructor';
+console.log(person);
+console.table(person);
+*/
+
+
+let val;
+
+/*val = String(202);
+//val = String(4+4);
+//val = String(true);
+//val = String(new Date());
+// val = (5).toString();
+//val = (true).toString();
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);*/
+
+val = Number(false);
+val = Number('hello');
+val = Number([1,2,3]);
+val = parseInt('100');
+val = parseFloat('100.32');
+val = parseInt([1,2,3]);
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed());
